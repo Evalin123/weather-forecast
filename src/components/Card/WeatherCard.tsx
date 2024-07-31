a4bead1a0ef972c7af5cb1201ba728e9225dcd91
Eva Lin <60597453+Evalin123@users.noreply.github.com>
2024-07-31 16:40:23 +0800

    feat: weather card style

--- src/components/Card/WeatherCard.tsx
+++ src/components/Card/WeatherCard.tsx
@@ -1,18 +1,37 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import Image from "next/image";
 
 type WeatherCardProps = {
   city: string;
   temperature: number;
   description: string;
+  icon: string;
 };
 
 const WeatherCard = (props: WeatherCardProps) => {
   return (
-    <div className="weather-card">
-      <h2>{props.city}</h2>
-      <p>Temperature: {props.temperature} °C</p>
-      <p>Description: {props.description}</p>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>{props.city}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Image
+          src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`}
+          width={48}
+          height={48}
+          alt="Weather Icon"
+        />
+        <p>{props.temperature} °C</p>
+        <p>{props.description}</p>
+      </CardContent>
+    </Card>
   );
 };
 
