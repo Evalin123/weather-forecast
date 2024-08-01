a58418273301ff2148d07bdd4091abe197c1506b
Eva Lin <60597453+Evalin123@users.noreply.github.com>
2024-08-01 17:25:33 +0800

    feat: get weather by input city name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,21 @@ import {
   GetWeatherByCoordsParams,
   getWeatherByCoordsQuery,
 } from "./api/weather/byCoords/route";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  city: z.string(),
+});
 
 export default function Home() {
-  const [city, setCity] = useState<string>();
+  const [city, setCity] = useState<GetWeatherByCityParams>();
   const [coords, setCoords] = useState<GetWeatherByCoordsParams>();
 
   const { data: cityWeather, isPending: isCityLoading } = useQuery({
-    ...getWeatherByCityQuery({ city: city }),
+    ...getWeatherByCityQuery({ ...city }),
     enabled: !!city,
   });
 
@@ -29,10 +37,7 @@ export default function Home() {
     isPending: isLocationLoading,
     isError: isLocationError,
   } = useQuery({
-    ...getWeatherByCoordsQuery({
-      latitude: coords?.latitude,
-      longitude: coords?.longitude,
-    }),
+    ...getWeatherByCoordsQuery({ ...coords }),
     enabled: !!coords && !!coords.latitude && !!coords.longitude,
   });
 
@@ -53,12 +58,40 @@ export default function Home() {
     }
   };
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      city: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    setCity({ ...values });
+  };
+
   return (
     <div>
       <h1 className="mb-4">Weather App</h1>
-      <div className="mb-5 flex w-full max-w-md items-center space-x-2">
-        <Input type="text" placeholder="Enter city name" />
-        <Button type="submit">Get Weather</Button>
+      <div className="mb-5 flex items-center space-x-2">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex space-x-2"
+          >
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter city name" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Get Weather</Button>
+          </form>
+        </Form>
         <Button variant="ghost" size="icon" onClick={getUserLocation}>
           <Navigation className="h-5 w-5" />
         </Button>
